8e8837a533079280ddf64b3d97574e03c6b9ec40
Miken37 <mike-166@hotmail.com>
2022-08-12 08:16:05 -0400

    Finished main playRound function that has conditional statements

--- code.js
+++ code.js
@@ -4,3 +4,52 @@ function getComputerChoice(){
     return cpuChoice = gestures[choiceNum];
 }
 
+function playRound(playerSelection, computerSelection){
+    switch (playerSelection){
+        case 'rock': 
+        if (computerSelection === 'paper'){
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        } else if (computerSelection === 'scissors'){
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        }
+        else{
+            console.log(`You tied with ${playerSelection} and ${computerSelection}`)
+        }
+        break;
+
+        case 'paper': 
+        if (computerSelection === 'scissors'){
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        } else if (computerSelection === 'rock'){
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        }
+        else{
+            console.log(`You tied with ${playerSelection} and ${computerSelection}`)
+        }
+        break;    
+
+        case 'scissors': 
+        if (computerSelection === 'rock'){
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        } else if (computerSelection === 'paper'){
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        }
+        else{
+            console.log(`You tied with ${playerSelection} and ${computerSelection}`)
+        }
+        break;
+
+        default:
+            console.log("You did not pick one of the three options. :(")
+    }
+
+}
+
+function game(){
+    
+}
+
+//alert("Welcome to Rock, Paper Scissors!")
+let playerSelection = prompt("Type in: Rock, Paper or Scissors", "").toLowerCase();
+let computerSelection = getComputerChoice();
+
