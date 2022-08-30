43985d55fb0b3f98b1a4b8d820ffad398380cc6b
Miken37 <mike-166@hotmail.com>
2022-08-30 06:57:44 -0400

    Event listeners in place, modified html and css to add new div for results

--- code.js
+++ code.js
@@ -48,11 +48,34 @@ function playRound(playerSelection, computerSelection){
 
 }
 
+
 const rock = document.querySelector(".rock")
-rock.addEventListener("click", () => console.log("Rock pressed"));
+rock.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    let playerSelection = "rock";
+    playRound(playerSelection, computerSelection)
+});
+
+const paper = document.querySelector(".paper")
+paper.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    let playerSelection = "paper";
+    playRound(playerSelection, computerSelection)
+});
+
+const scissors = document.querySelector(".scissors")
+scissors.addEventListener("click", () => {
+    let computerSelection = getComputerChoice();
+    let playerSelection = "scissors";
+    playRound(playerSelection, computerSelection)
+});
 
 
 
+
+
+
+//Old code from when game was on console only - left for reference
 /*function game(){
     for (let i = 0; i < 5; i++){
         let computerSelection = getComputerChoice();
